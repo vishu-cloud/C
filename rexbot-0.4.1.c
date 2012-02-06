ebebd9d75c77a3b6d20a41fb7a41c1b238793ef9
Cooler_ <c00f3r@gmail.com>
2012-02-06 12:56:29 -0200

    Just another IRC BOT in C

--- rexbot-0.4.1.c
+++ rexbot-0.4.1.c
@@ -7,14 +7,14 @@ to run follow this example; "gcc -o rexbot rexbot.c; ./rexBot ip port nick chan 
    Co-author: Victor Ramos Mello aka m0nad
    contact: m0nad[at]email[dot]com
 
-   Copyright 2007, 2008 by Antonio Costa. All Rights Reserved.
+    Copyright 2007, 2008 by Antonio Costa. All Rights Reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
-
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
